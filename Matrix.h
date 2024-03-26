194dcdea5679c6b9265d9ca9fc28d61b3426d5df
SediakinIvan <145792548+SediakinIvan@users.noreply.github.com>
2024-03-26 13:56:42 +0300

    Update Matrix.h

--- Matrix.h
+++ Matrix.h
@@ -1,25 +1,33 @@
-#ifndef MATRIX_H
+#pragma once      //директива обеспечивает защиту от многократного включения заголовочного файла
+#ifndef MATRIX_H  //начинает условную компиляцию, чтобы проверить, был ли файл уже включен ранее
 #define MATRIX_H
+
+#include <iostream>
 
 class Matrix {
 private:
-    int** data;
-    unsigned int m;
-    unsigned int n;
+    int** data;     //для хранепния данных матрицы
+    unsigned int m, n;   //для хранения количества строк и столбцов матрицы
 
 public:
-    Matrix(unsigned int rows, unsigned int cols);
-    ~Matrix();
-    int* operator[](unsigned int index);
+    Matrix(unsigned int rows, unsigned int columns);   //конструктор, создает матрицу заданного размера
+    ~Matrix();                                         //деструктор, освобождает динамически выделенную память
+
+    int* operator[](unsigned int i);
+
     void fillRandom();
-    friend std::ostream& operator<<(std::ostream& os, const Matrix& matrix);
-    Matrix operator+(const Matrix& otherMatrix) const;
-    Matrix& operator+=(const Matrix& otherMatrix);
-    Matrix operator-(const Matrix& otherMatrix) const;
-    Matrix& operator-=(const Matrix& otherMatrix);
-    Matrix operator*(const Matrix& otherMatrix) const;
-    bool operator==(const Matrix& otherMatrix) const;
-    bool operator!=(const Matrix& otherMatrix) const;
+
+    Matrix operator+(const Matrix& other);
+    Matrix operator-(const Matrix& other);
+    Matrix operator*(const Matrix& other);
+
+    Matrix& operator+=(const Matrix& other);
+    Matrix& operator-=(const Matrix& other);
+
+    bool operator==(const Matrix& other);
+    bool operator!=(const Matrix& other);
+
+    friend std::ostream& operator<<(std::ostream& out, const Matrix& matrix);
 };
 
 #endif
